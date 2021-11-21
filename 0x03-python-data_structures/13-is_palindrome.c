9f91b83e296f3b5309ca8a135186691a3fe22f69
heniarmoniyem <henokmesfin2121@gmail.com>
2021-11-21 17:36:18 +0000

    Update Palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,78 +1,35 @@
+#include <stddef.h>
+#include <stdio.h>
 #include "lists.h"
+
 /**
- * is_palindrome - Determines if a linked list is a palindrome
- * @head: Pointer to the entire linked list
- * Return: 1 if it is a palindrome, otherwise 0
+ * is_palindrome - check if a linked list is a palindrome
+ *
+ * @head: first node
+ *
+ * Return: 1 if success
+ *         0 if failed
  */
 int is_palindrome(listint_t **head)
 {
-  listint_t *h = NULL, *aux = NULL, *aux2 = NULL;
-  int size = 0, half = 0, i = 0, pal = 1;
+  listint_t *tmp = *head;
+  int values[2048], i = 0, cLoop, limit;
 
-  if (*head == NULL)
-    return (pal);
-  size = size_list(*head);
-  if (size == 1)
-    return (pal);
-  if (size == 2)
+  if (head == NULL || *head == NULL)
+    return (1);
+
+  while (tmp != NULL)
     {
-      if ((*head)->n != (*head)->next->n)
-	return (0);
-      else
-	return (pal);
-    }
-  half = size / 2;
-  aux = *head;
-  while (i < half)
-    {
+      values[i] = tmp->n;
       i++;
-      add_node_at_head(&h, aux->n);
-      aux = aux->next;
+      tmp = tmp->next;
     }
-  if (size % 2 != 0)
-    aux = aux->next;
-  aux2 = h;
-  while (aux != NULL)
-    {
-      if (aux->n != aux2->n)
-	pal = 0;
-      aux = aux->next;
-      aux2 = aux2->next;
-    }
-  free_listint(h);
-  return (pal);
-}
-/**
- * size_list - Gets the size of a list
- * @head: pointer to the first node of the structure
- * Return: Number of nodes
- */
-int size_list(listint_t *head)
-{
-  int i = 0;
 
-  while (head != NULL)
-    {
-      i++;
-      head = head->next;
-    }
-  return (i);
-}
-/**
- * add_node_at_head - add nodes from the original list as stack
- * @head: Structure of the stack
- * @n: Number to be added
- *Return: The created node
- */
-listint_t *add_node_at_head(listint_t **head, int n)
-{
-  listint_t *newnode;
+  limit = (i % 2 == 0) ? i / 2 : (i + 1) / 2;
 
-  newnode = malloc(sizeof(listint_t));
-  if (newnode == NULL)
-    return (NULL);
-  newnode->n = n;
-  newnode->next = *head;
-  *head = newnode;
-  return (*head);
+  for (cLoop = 0; cLoop < limit; cLoop++)
+    if (values[cLoop] != values[i - 1 - cLoop])
+      return (0);
+
+  return (1);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,78 +1,24 @@
-#include "lists.h"
-/**
- * is_palindrome - Determines if a linked list is a palindrome
- * @head: Pointer to the entire linked list
- * Return: 1 if it is a palindrome, otherwise 0
- */
-int is_palindrome(listint_t **head)
-{
-  listint_t *h = NULL, *aux = NULL, *aux2 = NULL;
-  int size = 0, half = 0, i = 0, pal = 1;
+#ifndef LISTS_H
+#define LISTS_H
 
-  if (*head == NULL)
-    return (pal);
-  size = size_list(*head);
-  if (size == 1)
-    return (pal);
-  if (size == 2)
-    {
-      if ((*head)->n != (*head)->next->n)
-	return (0);
-      else
-	return (pal);
-    }
-  half = size / 2;
-  aux = *head;
-  while (i < half)
-    {
-      i++;
-      add_node_at_head(&h, aux->n);
-      aux = aux->next;
-    }
-  if (size % 2 != 0)
-    aux = aux->next;
-  aux2 = h;
-  while (aux != NULL)
-    {
-      if (aux->n != aux2->n)
-	pal = 0;
-      aux = aux->next;
-      aux2 = aux2->next;
-    }
-  free_listint(h);
-  return (pal);
-}
 /**
- * size_list - Gets the size of a list
- * @head: pointer to the first node of the structure
- * Return: Number of nodes
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for project
  */
-int size_list(listint_t *head)
+typedef struct listint_s
 {
-  int i = 0;
+  int n;
+  struct listint_s *next;
+} listint_t;
 
-  while (head != NULL)
-    {
-      i++;
-      head = head->next;
-    }
-  return (i);
-}
-/**
- * add_node_at_head - add nodes from the original list as stack
- * @head: Structure of the stack
- * @n: Number to be added
- *Return: The created node
- */
-listint_t *add_node_at_head(listint_t **head, int n)
-{
-  listint_t *newnode;
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
-  newnode = malloc(sizeof(listint_t));
-  if (newnode == NULL)
-    return (NULL);
-  newnode->n = n;
-  newnode->next = *head;
-  *head = newnode;
-  return (*head);
-}
+int is_palindrome(listint_t **head);
+
+#endif /* LISTS_H */
